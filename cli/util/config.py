f64f8cdd9b40f62a5a73e6f5d79a9cebe3862e6b
penguineo <subham020304@gmail.com>
2025-05-08 01:38:02 +0530

    refactor: change load_config to return dict
    - Changed load_config() to return dictionary.
    - Returning the config as a dictionary will help to use it modularly.

--- cli/util/config.py
+++ cli/util/config.py
@@ -2,4 +2,13 @@ import yaml
 def load_config(path="config.yaml"):
     with open(path, "r") as f:
         config = yaml.safe_load(f)
+
+    for key, value in config.items():
+        if isinstance(value, str):
+            try:
+                config[key] = value.format(**config)
+            except KeyError as e:
+                raise ValueError(f"Missing key during interpolation: {e}")
+            except Exception as e:
+                raise RuntimeError(f"Error formatting key '{key}': {e}")
     return config
