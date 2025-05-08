83bed89d94165529af5dfe79174370b4617adef6
penguineo <subham020304@gmail.com>
2025-05-08 20:01:55 +0530

    feat: add switch-case logic for browser detection
    - Parsed config for browser_name and browser_settings.
    - Added switch-case logic for browser detection.
    - Integrated dynamic browser class instantiation in main.py.
    - Implemented GoogleChromeStable class for google-chrome-stable browser.

--- cli/main.py
+++ cli/main.py
@@ -1,3 +1,4 @@
+from browsers.google_chrome import GoogleChromeStable
 from util.config import load_config
 from util.log import configure_logger
 from util.log import logger as log
@@ -9,6 +10,21 @@ def main():
         config = load_config()
         configure_logger(log_level=config["log_level"])
         log.success("Starting application.")
+        browser_config: dict = config.get("browsers", {})
+        for browser_name, browser_settings in browser_config.items():
+            if not browser_settings.get("enabled", False):
+                continue
+            profiles = browser_settings.get("profiles")
+            match browser_name:
+                case "google-chrome-stable":
+                    chrome_stable = GoogleChromeStable
+                    if chrome_stable.is_installed:
+                        data = chrome_stable.get_bookmarks(profiles)
+                case "firefox":
+                    pass
+                case _:
+                    log.warning(f"Unsupported browser: {browser_name}")
+                    continue
     except Exception as e:
         log.error(f"Starting application: {e}")
         raise
