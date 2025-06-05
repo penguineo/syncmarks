7bf96feb7655b5254bd84aa1bedd73fb58897598
penguineo <subham020304@gmail.com>
2025-06-05 06:13:01 +0530

    refactor: change log print of load_config in config.py

--- cli/util/config.py
+++ cli/util/config.py
@@ -9,9 +9,8 @@ from util.log import logger as log
 
 def load_config(path: Path):
     log.info(
-        "Starting to load config",
-        extra={"function": load_config.__name__, "config_path": path},
-    )
+        "Loading config",
+        extra={"function": load_config.__name__, "config_path": path},)
 
     with open(path, "r") as f:
         config = yaml.safe_load(f)
@@ -25,7 +24,7 @@ def load_config(path: Path):
             except Exception as e:
                 raise RuntimeError(f"Error formatting key '{key}': {e}")
     log.success(
-        "Starting to load config",
+        "Loading config",
         extra={"function": load_config.__name__, "config_path": path},
     )
     return config
