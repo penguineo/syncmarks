220aae7bab8f1c33047316b0a0a46cfd6ef4d54c
penguineo <subham020304@gmail.com>
2025-06-11 13:14:33 +0000

    feat: add handle_verbosity to handler.py

--- cli/handler.py
+++ cli/handler.py
@@ -1,9 +1,19 @@
 from argparse import Namespace
 
 from util.config import config_path, generate_sample_config
+from util.log import configure_logger
+
+
 def handle_args(args: Namespace):
+    verbosity_level = args.verbose
+    if verbosity_level:
+        handle_verbose(verbosity_level)
     if hasattr(args, "func"):
         args.func()
+
+
+def handle_verbose(verbosity_level: str):
+    configure_logger(verbosity_level)
 
 
 def handle_generate_sample_parser():
